4ad89c184ba2b8dc4b9aeefe1497798b3cc47963
Martín Aguilar Tello <tello@072004.xyz>
2025-12-04 16:55:00 -0600

    api: Agregar utilidades para manejar licencias

--- api/license.go
+++ api/license.go
@@ -1,6 +1,9 @@
 package edutrack
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"time"
 
 	"gorm.io/gorm"
@@ -27,7 +30,7 @@ const (
 type License struct {
 	gorm.Model
 
-	// Unique license key.
+	// Unique license key (used for institutional login).
 	Key string `gorm:"uniqueIndex"`
 
 	// Type of license (trial, basic, pro, enterprise).
@@ -60,4 +63,89 @@ func (l *License) IsExpired() bool {
 // IsValid returns true if the license is active and not expired.
 func (l *License) IsValid() bool {
 	return l.Active && !l.IsExpired()
+}
+
+// GenerateLicenseKey generates a new unique license key.
+// Format: XXXX-XXXX-XXXX-XXXX (16 hex characters with dashes).
+func GenerateLicenseKey() (string, error) {
+	bytes := make([]byte, 8)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", fmt.Errorf("failed to generate license key: %w", err)
+	}
+
+	hex := hex.EncodeToString(bytes)
+	return fmt.Sprintf("%s-%s-%s-%s",
+		hex[0:4],
+		hex[4:8],
+		hex[8:12],
+		hex[12:16],
+	), nil
+}
+
+// NewLicense creates a new license with default values.
+func NewLicense(licenseType LicenseType, duration time.Duration) (*License, error) {
+	key, err := GenerateLicenseKey()
+	if err != nil {
+		return nil, err
+	}
+
+	license := &License{
+		Key:      key,
+		Type:     licenseType,
+		ExpiryAt: time.Now().Add(duration),
+		Active:   true,
+	}
+
+	// Set limits based on license type.
+	switch licenseType {
+	case LicenseTypeTrial:
+		license.MaxUsers = 3
+		license.MaxStudents = 25
+		license.MaxCourses = 5
+	case LicenseTypeBasic:
+		license.MaxUsers = 10
+		license.MaxStudents = 100
+		license.MaxCourses = 20
+	case LicenseTypePro:
+		license.MaxUsers = 50
+		license.MaxStudents = 500
+		license.MaxCourses = 100
+	case LicenseTypeEnterprise:
+		license.MaxUsers = -1 // Unlimited
+		license.MaxStudents = -1
+		license.MaxCourses = -1
+	}
+
+	return license, nil
+}
+
+// Regenerate generates a new key for an existing license and optionally extends it.
+func (l *License) Regenerate(extendDuration time.Duration) error {
+	key, err := GenerateLicenseKey()
+	if err != nil {
+		return err
+	}
+
+	l.Key = key
+	l.Active = true
+
+	if extendDuration > 0 {
+		// Extend from now or from expiry date, whichever is later.
+		baseTime := time.Now()
+		if l.ExpiryAt.After(baseTime) {
+			baseTime = l.ExpiryAt
+		}
+		l.ExpiryAt = baseTime.Add(extendDuration)
+	}
+
+	return nil
+}
+
+// DaysUntilExpiry returns the number of days until the license expires.
+// Returns 0 if already expired.
+func (l *License) DaysUntilExpiry() int {
+	if l.IsExpired() {
+		return 0
+	}
+	return int(time.Until(l.ExpiryAt).Hours() / 24)
 }
